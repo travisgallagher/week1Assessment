b65583788956d377de14eb3d4fb94110bbcf55c9
travisgallagher <travisgallagher52@gmail.com>
2022-02-11 10:30:36 -0700

    Initial commit

---
+++ sldc.js
@@ -0,0 +1,39 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Analysis of Requirements
+// Design
+// Implementation
+// Maintain
+// Planning
+// Testing & Integration
+
+
+// Planning 
+// Analysis of Requirements 
+// Design
+// Implementation
+// Testing & Integration 
+// Maintain
+
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." ;
+
+const analysisOfRequirements = "In this stage of the SDL, you will analyze what the end-goal is. You will determine what the expectations of the user/shareholder are for the project and what resources you will need to meet those expectations."; 
+
+const design = "In this stage, we will create a blue print for the product. This will include how we will accomplish our expectations. It will also help all parts of a team have a structure to see where they are in the project as a whole."; 
+
+const implementation = "This is the stage we will start coding. We will take our blueprint and begin to implement the ideas into code. It's also important to set due dates and deadlines to ensure we will finish on time.";
+
+const testingIntegration = "Here we will test our code, and make sure all of the features work how we want them to work.";
+
+const maintain = "At this point in the SDL we have our first version out and being used. Now our job is to maintain and fix bugs that arise.";
